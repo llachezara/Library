e3a397d5feff4447c50144550269597755bfb6d4
Lachezara <93489868+llachezara@users.noreply.github.com>
2024-10-14 22:51:49 +0300

    Fix tests

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -186,7 +186,6 @@ test('Verify redirection of Logout link after user login', async ({ page }) => {
 
   const logoutLink = await page.$('a[href="javascript:void(0)"]');
   await logoutLink.click();
-  await page.waitForURL('http://localhost:3000/catalog');
 
   const redirectedURL = page.url();
   expect(redirectedURL).toBe('http://localhost:3000/catalog');
